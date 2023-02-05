0fa149ce7c1cc6aab0fc8b67405d1150c552ad93
WillowSauceR <WillowSauceR@outlook.com>
2023-02-05 16:33:05 +0800

    correct version

--- mod/Version.h
+++ mod/Version.h
@@ -3,4 +3,4 @@ constexpr unsigned PYR_MAJOR_VERSION = 1;
 constexpr unsigned PYR_MINOR_VERSION = 19;
 constexpr unsigned PYR_MICRO_VERSION = 52;
 constexpr const char* PYR_EXTRA_VERSION = "a";
-constexpr const char* PYR_VERSION = "v1.9.52a";
+constexpr const char* PYR_VERSION = "v1.19.52a";
