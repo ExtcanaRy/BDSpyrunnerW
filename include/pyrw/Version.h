bfa398bed717e6626ccf431972aaecd613acfced
WillowSauceR <WillowSauceR@outlook.com>
2023-03-28 13:55:24 +0800

    Release v2.1.7 Support BDS 1.19.72.01

--- include/pyrw/Version.h
+++ include/pyrw/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 constexpr unsigned PYR_MAJOR_VERSION = 2;
 constexpr unsigned PYR_MINOR_VERSION = 1;
-constexpr unsigned PYR_MICRO_VERSION = 6;
+constexpr unsigned PYR_MICRO_VERSION = 7;
 constexpr const char* PYR_EXTRA_VERSION = "";
-constexpr const char* PYR_VERSION = "v2.1.6";
+constexpr const char* PYR_VERSION = "v2.1.7";
